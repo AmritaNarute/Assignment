1d66995f16381191da0a3f5ce7c7e0eef8f4b000
DWebPixel <dwebpixel@DWebPixels-MacBook-Pro.local>
2024-04-15 10:56:31 +0530

    initial Commit Assignment

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useMemo, useCallback} from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import ProductDetails from './src/components/ProductDetails'
 
-export default function App() {
+const App = () => {
+  const [product, setProduct] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/products',
+      {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+      const data = await response.json();
+      // console.log(data)
+      setProduct(data.products);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // console.log("state:", product)
+
+  const computeDetails = useMemo(() => (item) => {
+    return `ID: ${item.id}, Title: ${item.title}`;
+  }, []);
+
+  // Callback function for selecting a product
+  const selectProduct = useCallback((productId) => {
+    setSelectedProduct(productId);
+  }, []);
+
+
+  const renderProductItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectProduct(item.id)}>
+      <View style={styles.card}>
+        <Text>{computeDetails(item)}</Text>
+      </View>
+
+    {/* <View style={styles.card}>
+      <Text style={styles.id}>ID: {item.id}</Text>
+      <Text style={styles.title}>Title: {item.title}</Text>
+    </View> */}
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+       showsVerticalScrollIndicator={false}
+        data={product}
+        renderItem={renderProductItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+      {selectedProduct && (
+        <ProductDetails productId={selectedProduct} />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 10,
+    marginTop:10
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#fff', 
+  },
+  id: {
+    fontWeight: 'bold',
+  },
+  title: {
+    marginTop: 5,
   },
 });
+
+export default App
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProductDetails = ({ productId }) => {
+    const [productDetails, setProductDetails] = useState(null);
+
+    useEffect(() => {
+      // Fetch product details using the product ID
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`https://dummyjson.com/products/${productId}`);
+          const data = await response.json();
+          setProductDetails(data); // Set product details to state
+        } catch (error) {
+          console.error('Error fetching product details:', error);
+        }
+      };
+  
+      fetchData();
+    }, [productId]);
+  
+    useEffect(() => {
+      console.log('Child component re-rendered due to changes in props.');
+    });
+    console.log("ProductInfo:",productDetails)
+  
+    return (
+      <View style={styles.container}>
+          <View>
+            <Text>Product Details for ID: {productDetails.id} {productDetails.title}</Text>
+            {/* Display other product details here */}
+          </View>
+       
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      padding: 10,
+      borderTopWidth: 1,
+      borderTopColor: '#ccc',
+    },
+  });
+  
+  export default ProductDetails;
